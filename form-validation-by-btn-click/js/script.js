11f3b0be1e55b70817b9b3802f305964fd55836a
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-11-27 23:28:34 +0200

    ++

---
+++ form-validation-by-btn-click/js/script.js
@@ -0,0 +1,103 @@
+const form = document.getElementById('form'),
+  allDivs = document.querySelectorAll('.form-control'),
+  username = document.getElementById('username'),
+  email = document.getElementById('email'),
+  password = document.getElementById('Password'),
+  password2 = document.getElementById('password2'),
+  popup = document.querySelector('.modal-overlay');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  //get the values from the inputs
+
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === '') {
+    //show error
+    //add error class
+    setErrorFor(username, 'Username cannot be blank');
+  } else if (!isLettersOnly(usernameValue)) {
+    setErrorFor(username, 'Should be letters only');
+  } else {
+    //add success class
+    setSuccessFor(username);
+  }
+
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Email is not valid');
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === '') {
+    //show error
+    //add error class
+    setErrorFor(password, 'Password cannot be blank');
+  } else {
+    //add success class
+    setSuccessFor(password);
+  }
+
+  if (password2Value === '') {
+    //show error
+    //add error class
+    setErrorFor(password2, 'Password2 cannot be blank');
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(password2, 'Passwords does not match');
+  } else {
+    //add success class
+    setSuccessFor(password2);
+  }
+
+  //show a success message
+
+  checkAllValidInputs(allDivs);
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; //.form-control;
+  const small = formControl.querySelector('small');
+
+  //add error message inside small
+  small.innerText = message;
+
+  //add error class
+  formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  //add error succes
+  formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+}
+function isLettersOnly(userName) {
+  return /^[A-Za-z]+$/.test(userName);
+}
+
+function checkAllValidInputs(allInputs) {
+  console.log(allInputs[2]);
+
+
+  allInputs.forEach((item) => {
+    console.log(item);
+    // if (item.classList.contains('success')) {
+    //   //counter++;
+    //   console.log('сюда зашёл');
+    // } // popup.classList.add('open-modal');
+  });
+}
