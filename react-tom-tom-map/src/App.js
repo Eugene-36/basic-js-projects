43ccb8c759a7898574a3775529a5c68ee7467eb1
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-01-19 21:39:29 +0200

    long, latitude

--- react-tom-tom-map/src/App.js
+++ react-tom-tom-map/src/App.js
@@ -1,8 +1,29 @@
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
+import * as tt from '@tomtom-international/web-sdk-maps';
 
 const App = () => {
-  console.log('work');
-  return <div className='App'></div>;
+  const mapElement = useRef();
+  const [map, setMap] = useState({});
+  const longitude = -0.112869;
+  const latitude = 51.504;
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+      center: [longitude, latitude],
+      zoom: 14,
+    });
+
+    setMap(map);
+  }, []);
+
+  return (
+    <div className='app'>
+      <div ref={mapElement} className='map'></div>
+    </div>
+  );
 };
 
 export default App;
