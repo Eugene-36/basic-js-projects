63631631b7233693023684585ed5481de607bbb0
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-02-17 14:53:18 +0200

    done with animation

--- cart-js-filtration/main.js
+++ cart-js-filtration/main.js
@@ -1,1 +1,38 @@
+function app() {
+  const buttons = document.querySelectorAll('.button');
+  const cards = document.querySelectorAll('.card');
+
+  function filter(category, items) {
+    items.forEach((item) => {
+      const isItemFiltered = !item.classList.contains(category);
+      const isShowAll = category.toLowerCase() === 'all';
+      console.log();
+      if (isItemFiltered && !isShowAll) {
+        item.classList.add('anime');
+      } else {
+        item.classList.remove('hide');
+        item.classList.remove('anime');
+      }
+    });
+  }
+
+  //! Вешаем обработчик на каждую из карточек и получаем dataset
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const currentCategory = button.dataset.filter;
+      filter(currentCategory, cards);
+    });
+  });
+
+  cards.forEach((card) => {
+    card.ontransitionend = function () {
+      if (card.classList.contains('anime')) {
+        card.classList.add('hide');
+        console.log('Анимация ЗАВЕРШЕНА!');
+      }
+    };
+  });
+}
+
+app();
 console.log('work fine');
