a271e61bd1e825f96f43c313c45521ed98643e5c
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-02-25 19:47:29 +0200

    ++

--- advanced-js/script.js
+++ advanced-js/script.js
@@ -1,1 +1,57 @@
-console.log('work fine and properly');
+// let a = 10;
+
+// function outer() {
+//   let b = 20;
+
+//   function inner() {
+//     let c = 30;
+
+//     console.log(a, b, c);
+//   }
+
+//   inner();
+// }
+
+// outer();
+
+//! Scope ===============
+// function outer() {
+//   let counter = 0;
+
+//   function inner() {
+//     counter++;
+//     console.log(counter);
+//   }
+//   return inner;
+// }
+
+// const fn = outer();
+// fn();
+// fn();
+
+//* ====================
+
+// function Person(fName, lName) {
+//   this.firstName = fName;
+//   this.lastName = lName;
+// }
+
+// Person.prototype.getFullName = function () {};
+
+// function SuperHero(fName, lName) {
+//   this.isSuperHero = true;
+// }
+// SuperHero.prototype.fightCrime = function () {
+//   console.log('Fighting crime');
+// };
+
+//const p1 = new Person('Vishwas');
+//const p2 = new Person('Batman');
+
+//===========================================
+const obj = {
+  [Symbol.iterator]: function () {
+    const iterator = {};
+    return iterator;
+  },
+};
