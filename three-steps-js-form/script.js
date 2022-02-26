e215b76b713c43a49c75965fa4ad9ca27477b113
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-02-26 22:39:13 +0200

    flex fo elements

--- three-steps-js-form/script.js
+++ three-steps-js-form/script.js
@@ -7,8 +7,47 @@ let currentStep = formSteps.findIndex((step) => {
 
 if (currentStep < 0) {
   currentStep = 0;
-  formSteps[currentStep].classList.add('active');
+  showCurrentStep();
 }
 
-console.log('currentStep', currentStep);
-console.log('currentStep', currentStep);
+// console.log('currentStep', currentStep);
+// console.log('currentStep', currentStep);
+
+multiStepForm.addEventListener('click', (e) => {
+  let incrementor;
+
+  if (e.target.matches('[data-next]')) {
+    incrementor = 1;
+  } else if (e.target.matches('[data-previous]')) {
+    incrementor = -1;
+  }
+  if (incrementor == null) return;
+
+  const inputs = [...formSteps[currentStep].querySelectorAll('input')];
+
+  const allValid = inputs.every((input) => input.reportValidity());
+
+  console.log('inputs', inputs);
+
+  console.log('allValid', allValid);
+
+  if (allValid) {
+    currentStep += incrementor;
+    showCurrentStep();
+  }
+  //console.log('inputs', inputs);
+});
+
+formSteps.forEach((step) => {
+  step.addEventListener('animationend', (e) => {
+    formSteps[currentStep].classList.remove('hide');
+    e.target.classList.toggle('hide', !e.target.classList.contains('active'));
+  });
+});
+
+function showCurrentStep() {
+  formSteps.forEach((step, index) => {
+    //console.log('step', step);
+    step.classList.toggle('active', index === currentStep);
+  });
+}
