0af6e58912b138ad46c29c3e1c429c5fff7281c1
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-03-07 22:23:27 +0200

    changed database

--- mysql-nodejs-project/app.js
+++ mysql-nodejs-project/app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'eguene',
-  database: 'sql_course',
+  database: 'user',
   password: process.env.PASSWORD_KEY,
 });
 
